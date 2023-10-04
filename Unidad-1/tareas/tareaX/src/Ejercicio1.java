8ba8452e78d7b12a3d9c5327d62f0233966a4e85
Shbarroso <samuelaurix2@gmail.com>
2023-10-04 10:46:28 +0100

    Se crea la tarea1

--- Unidad-1/tareas/tareaX/src/Ejercicio1.java
+++ Unidad-1/tareas/tareaX/src/Ejercicio1.java
@@ -1,13 +1,28 @@
-/**
- * @author Nombre del autor
- */
-public class Ejercicio1 {
+<div align="justify">
 
-    /**
-     * Descripcion del algoritmo
-     * @param args
-     */
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
+        # Tarea
+
+        - [Tarea 1](#tarea 1)
+
+        ## Tarea 1 <a name = "tarea 1"></a>
+        Desarrolle un algoritmo que permita calcular Promedio de Notas; finaliza cuando N = 0.
+
+        ### Diagrama de Flujo
+
+
+
+
+        ### pseudocodigo
+
+        Inicio
+        - Declaración de Variables:
+        N = 0, Promedio = 0, Acumula= 0
+        -  Leer N
+        - Mientras N <> 0 hacer
+        -  Cuenta = Cuenta + 1
+        -  Acumula = Acumula + N
+        -  Fin Mientras
+        -  Promedio = Acumula/Cuenta
+        -  Imprimir “Promedio:”; Promedio
+        -  Fin
+</div>
