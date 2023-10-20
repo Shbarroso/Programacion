efd9aac45ed182bc7e3a70b6e7adcb4f709eac34
Shbarroso <samuelaurix2@gmail.com>
2023-10-20 19:14:09 +0100

    Se sube la tarea funcion

---
+++ Unidad-1/tarea-funciones/src/main/java/ies/puerto/Ejercicio1.java
@@ -0,0 +1,20 @@
+package ies.puerto;
+
+public class Ejercicio1 {
+
+    /**
+     * Función que realiza el calculo del área en triangulo
+     * @param base para realizar el calculo
+     * @param altura para realizar el calculo
+     * @return areaTriangulo dado
+     */
+    public int areaTriangulo(int base, int altura) {
+        int resultado = 0;
+        resultado = (base*altura)/2;
+
+
+
+        return resultado;
+    }
+
+}
---
+++ Unidad-1/tarea-funciones/src/main/java/ies/puerto/Ejercicio2.java
@@ -0,0 +1,14 @@
+package ies.puerto;
+
+public class Ejercicio2 {
+
+    public double areaCirculo(int radio) {
+        double resultado = 0;
+        double pi = Math.PI;
+
+        resultado = Math.pow(radio, 2)*pi;
+
+
+        return resultado;
+    }
+}
