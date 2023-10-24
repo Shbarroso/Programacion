e86965fb91fde20484f24fbdd49a771e2327cb0b
Shbarroso <samuelaurix2@gmail.com>
2023-10-24 11:36:02 +0100

    Se actualiza ejercicio1

--- trabajo-metodos-2/src/main/java/ies/puerto/Ejercicio1.java
+++ trabajo-metodos-2/src/main/java/ies/puerto/Ejercicio1.java
@@ -3,6 +3,8 @@ package ies.puerto;
 public class Ejercicio1 {
     public int calcularSueldoHoras( int numerosHoras) {
         int sueldo = 0;
+        int sueldo = numerosHoras *10;
+        int horasTrabajada = 160;
 
 
         return sueldo;
